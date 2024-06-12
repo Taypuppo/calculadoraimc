6fdbf36b34850c733dff3bae4aa4de846e36c8d9
Taypuppo <taypuppo@icloud.com>
2024-06-12 17:55:32 -0300

    Melhorando a experiencia do usuario

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -58,17 +58,11 @@ class MainActivity : AppCompatActivity() {
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
 
-                // Navegar para a próxima tela
-                // Criar o layout da próxima tela
-                // Passar dados(resultado) para a próxima tela
-
                 // Intent - Classe do próprio android
 
                 val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra (KEY_RESULT_IMC, resultado)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
                 startActivity(intent)
-
-                println("testando peso" + resultado)
 
             }
         }
